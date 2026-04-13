391ebca155681888e4b11c06a0378f6592a1a9d1
haidang <tieuhaidang1@gmail.com>
2026-04-13 18:26:07 +0700

    create common ApiResponse and Setup Database

--- trac-nghiem-api/src/app.module.ts
+++ trac-nghiem-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com',
+      port: 4000,
+      username: '2k573cTqtncSS2K.root',
+      password: 'p6pgN88fvWMx8uUd',
+      database: 'TracNghiem',
+      entities: [], // Danh sách các entity sẽ ánh xạ
+      synchronize: true, // Tự động tạo bảng từ entity (trong development)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
