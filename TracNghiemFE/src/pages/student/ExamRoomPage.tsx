109e9e6fd3fc6ca15aad63d0019e38ad5dfe8f6e
haidang <tieuhaidang1@gmail.com>
2026-04-21 07:31:01 +0700

    tạo cấu trúc dự án FE

---
+++ TracNghiemFE/src/pages/student/ExamRoomPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ExamRoomPage() {
+  return (
+    <div>ExamRoomPage</div>
+  )
+}
+
+export default ExamRoomPage
---
+++ TracNghiemFE/src/pages/teacher/sessions/SessionListPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SessionListPage() {
+  return (
+    <div>SessionListPage</div>
+  )
+}
+
+export default SessionListPage
